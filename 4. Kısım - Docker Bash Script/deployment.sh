cc4397b0ed0ad40b04213b3fc7136cebeadd22ac
Bayram_Ozkan <bayram_0310@hotmail.com>
2024-08-05 18:35:38 +0300

    Script Banner added.

--- 4. Kısım - Docker Bash Script/deployment.sh
+++ 4. Kısım - Docker Bash Script/deployment.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-echo "Docker Bash Script çalıştırılıyor "
+
+#!/bin/bash
+
+# Banner
+echo "***********************************************"
+echo "*                                             *"
+echo "*    TECHCAREER DEVOPS BİTİRME PROJESİ        *"
+echo "*                                             *"
+echo "*    Docker Bash Script çalıştırılıyor ...    *"
+echo "*                                             *"
+echo "*                                             *"
+echo "*           BAYRAM OZKAN                      *"
+echo "*                                             *"
+echo "*      https://github.com/nakzoo              *"
+echo "*                                             *"
+echo "***********************************************"
+
 
 
 docker-compose up -d 
