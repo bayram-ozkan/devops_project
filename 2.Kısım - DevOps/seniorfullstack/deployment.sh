d9e7c2813cd295bd85678458773f75721857c455
Bayram_Ozkan <bayram_0310@hotmail.com>
2024-08-06 21:38:07 +0300

    37.Adım file added

---
+++ 2.Kısım - DevOps/seniorfullstack/deployment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Banner
+echo "***********************************************"
+echo "*                                             *"
+echo "*    TECHCAREER DEVOPS BİTİRME PROJESİ        *"
+echo "*                                             *"
+echo "*            DevOps 37.Adım                   *"
+echo "*                                             *"
+echo "*                                             *"
+echo "*           BAYRAM OZKAN                      *"
+echo "*                                             *"
+echo "*      https://github.com/nakzoo              *"
+echo "*                                             *"
+echo "***********************************************"
+
+
+
+docker-compose up -d 
+
+
+
+
