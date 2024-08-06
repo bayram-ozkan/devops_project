25d1e41cbbbe63ecd28c038aad6f63793111d0e0
Bayram_Ozkan <bayram_0310@hotmail.com>
2024-08-06 13:03:13 +0300

    SonarQube

---
+++ 5. Kısım - Docker-compose & SonarQube/SonarQube/sonarqube-deployment.sh
@@ -0,0 +1,5 @@
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=zaman-cizelge-sistemi \
+  -Dsonar.projectName='zaman-cizelge-sistemi' \
+  -Dsonar.host.url=http://192.168.32.34:1111 \
+  -Dsonar.token=sqp_f70728d323feb1d958534953458fb3ef2c95e438
