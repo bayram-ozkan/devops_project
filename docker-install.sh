7507facd95d5154f23cf7fd91d61e53e0c2bc270
Bayram_Ozkan <bayram_0310@hotmail.com>
2024-08-09 13:19:36 +0300

    docker script add and revize

---
+++ docker-install.sh
@@ -0,0 +1,41 @@
+#https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+
+
+#!/bin/bash
+
+# Banner
+echo "***********************************************"
+echo "*                                             *"
+echo "*            Docker kurulumu                  *"
+echo "*                                             *"
+echo "*   Docker  kurulum script i çalıştırıldı     *"
+echo "*                                             *"
+echo "*          ******************                 *"
+echo "*                                             *"
+echo "*       https://github.com/nakzoo             *"
+echo "*                                             *"
+echo "***********************************************"
+
+
+sudo apt-get update
+
+sudo apt-get upgrade -y
+
+sudo apt-get install ca-certificates curl -y
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	
+apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo usermod -aG docker $USER
